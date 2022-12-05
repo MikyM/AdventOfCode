2724428f2d1b5fd7c1b832798da4ef15253f6b60
Krzysztof Kupisz <kupisz.krzysztof@gmail.com>
2022-12-05 19:08:57 +0100

    day5

---
+++ 2022/AdventOfCode2022/Day5/SupplyStacksDay5.cs
@@ -0,0 +1,202 @@
+﻿using System.Collections;
+using System.Text.Json;
+using System.Text.RegularExpressions;
+
+namespace AdventOfCode2022.Day5;
+
+public partial class SupplyStacksDay5
+{
+    public static async Task<(string Part1, string Part2)> SolveAsync()
+    {
+        var input = await File.ReadAllTextAsync(
+            Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Day5", "input.txt"));
+
+        var split = input.Split('\n', StringSplitOptions.RemoveEmptyEntries);
+        
+        var unparsedSupplyStacks = split.TakeWhile(x => !x.Contains("move")).ToArray();
+        var unparsedInstructions = split.Skip(unparsedSupplyStacks.Length);
+
+        var supplyStacks1 = new SupplyStacks(unparsedSupplyStacks);
+        var supplyStacks2 = new SupplyStacks(supplyStacks1);
+        var instructions = new CraneInstructionSet(unparsedInstructions);
+
+        ICrane crane9000 = new CrateMover9000();
+        crane9000.MoveCrates(ref supplyStacks1, ref instructions);
+        
+        ICrane crane9001 = new CrateMover9001();
+        crane9001.MoveCrates(ref supplyStacks2, ref instructions);
+
+        var part1 = string.Join(string.Empty, supplyStacks1.OrderBy(x => x.Key).Select(x => x.Value.Peek().Id));
+        var part2 = string.Join(string.Empty, supplyStacks2.OrderBy(x => x.Key).Select(x => x.Value.Peek().Id));
+        return (part1, part2);
+    }
+
+    public interface ICrane
+    {
+        void MoveCrates(ref SupplyStacks supplyStacks, ref CraneInstructionSet instructionSet);
+    }
+    
+    public sealed class CrateMover9001 : ICrane
+    {
+        public void MoveCrates(ref SupplyStacks supplyStacks, ref CraneInstructionSet instructionSet)
+        {
+            foreach (var instruction in instructionSet)
+            {
+                if (!supplyStacks.TryGetValue(instruction.OriginStackId, out var originStack))
+                {
+                    Console.WriteLine(JsonSerializer.Serialize(instruction));
+                    throw new InvalidOperationException();
+                }
+
+                if (!supplyStacks.TryGetValue(instruction.DestinationStackId, out var destinationStack))
+                {
+                    Console.WriteLine(JsonSerializer.Serialize(instruction));
+                    throw new InvalidOperationException();
+                }
+
+                var popped = new List<SupplyCrate>();
+                for (var i = 0; i < instruction.CrateCount; i++)
+                {
+                    if (originStack.TryPop(out var crate))
+                        popped.Add(crate);
+                }
+
+                popped.Reverse();
+                foreach (var crate in popped)
+                    destinationStack.Push(crate);
+            }
+        }
+    }
+    
+    public sealed class CrateMover9000 : ICrane
+    {
+        public void MoveCrates(ref SupplyStacks supplyStacks, ref CraneInstructionSet instructionSet)
+        {
+            foreach (var instruction in instructionSet)
+            {
+                if (!supplyStacks.TryGetValue(instruction.OriginStackId, out var originStack))
+                {
+                    Console.WriteLine(JsonSerializer.Serialize(instruction));
+                    throw new InvalidOperationException();
+                }
+
+                if (!supplyStacks.TryGetValue(instruction.DestinationStackId, out var destinationStack))
+                {
+                    Console.WriteLine(JsonSerializer.Serialize(instruction));
+                    throw new InvalidOperationException();
+                }
+
+                for (var i = 0; i < instruction.CrateCount; i++)
+                {
+                    if (originStack.TryPop(out var crate))
+                        destinationStack.Push(crate);
+                }
+            }
+        }
+    }
+    
+    public sealed partial class SupplyStacks : Dictionary<int,SupplyStack>
+    {
+        [GeneratedRegex(@"^[0-9\s\[\]]*$")]
+        private static partial Regex ExtractIndexLineRegex();
+
+        public SupplyStacks(SupplyStacks stacks)
+        {
+            foreach (var (_, stack) in stacks)
+            {
+                TryAdd(stack.Id, new SupplyStack(stack) );
+            }
+        }
+        
+        public SupplyStacks(IEnumerable<string> lines)
+        {
+            var enumerated = lines.ToList();
+
+            // init parse
+            var indexLine = enumerated.First(x => ExtractIndexLineRegex().IsMatch(x));
+            var indexes = indexLine.Where(char.IsNumber).ToDictionary(key => indexLine.IndexOf(key), value => value - '0');
+            var initialStackState = enumerated.TakeWhile(x => x.Contains('['));
+
+            // actual parse
+            var stacks = indexes.ToDictionary(x => x.Key, x => new SupplyStack { Id = x.Value });
+            
+            foreach (var line in initialStackState.Reverse().ToArray())
+            {
+                for (var i = 0; i < line.Length; i++)
+                {
+                    if (!char.IsLetter(line[i]))
+                        continue;
+                    
+                    stacks[i].Push(new SupplyCrate { Id = line[i] } );
+                }
+            }
+            
+            foreach (var (_, stack) in stacks)
+            {
+                TryAdd(stack.Id, stack);
+            }
+        }
+        
+    }
+    
+    public readonly partial struct CraneInstructionSet : IEnumerable<CraneInstruction>
+    {
+        private readonly IEnumerable<CraneInstruction> _underlyingEnumerable;
+
+        [GeneratedRegex(@"\d+")]
+        private static partial Regex ExtractNumbersRegex();
+        
+        public CraneInstructionSet(IEnumerable<string> unparsedInstructions)
+        {
+            var actual = new List<CraneInstruction>();
+            foreach (var line in unparsedInstructions)
+            {
+                var matches = ExtractNumbersRegex().Matches(line);
+                actual.Add(new CraneInstruction
+                {
+                    CrateCount = int.Parse(matches[0].Value), 
+                    OriginStackId = int.Parse(matches[1].Value), 
+                    DestinationStackId = int.Parse(matches[2].Value)
+                });
+            }
+            _underlyingEnumerable = actual;
+        }
+
+        public IEnumerator<CraneInstruction> GetEnumerator()
+            => _underlyingEnumerable.GetEnumerator();
+
+        IEnumerator IEnumerable.GetEnumerator()
+            => _underlyingEnumerable.GetEnumerator();
+    }
+
+    public readonly struct CraneInstruction
+    {
+        public int OriginStackId { get; init; }
+        public int DestinationStackId { get; init; }
+        public int CrateCount { get; init; }
+    }
+    
+    public readonly struct SupplyCrate
+    {
+        public char Id { get; init; }
+    }
+    
+    public sealed class SupplyStack : Stack<SupplyCrate>
+    {
+        public SupplyStack()
+        {
+        }
+        
+        public SupplyStack(SupplyStack stack)
+        {
+            Id = stack.Id;
+            
+            foreach (var crate in stack.Reverse())
+            {
+                Push(crate);
+            }
+        }
+        
+        public int Id { get; init; }
+    }
+}
--- 2022/AdventOfCode2022/Program.cs
+++ 2022/AdventOfCode2022/Program.cs
@@ -1,7 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 using AdventOfCode2022.Day4;
+using AdventOfCode2022.Day5;
 
-Console.WriteLine(await SectionsDay4.SolvePart1Async());
-Console.WriteLine(await SectionsDay4.SolvePart2Async());
+Console.WriteLine(await SupplyStacksDay5.SolveAsync());
 Console.ReadKey();
